7c00b1bba3048e93098647ca4811ea611357509d
crush3dice <frederick.altrock@web.de>
2021-05-25 01:19:48 +0200

    excluded mac from detectPhone.js

--- assets/js/detectPhone.js
+++ assets/js/detectPhone.js
@@ -1,3 +1,3 @@
-if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 	$(".sidebar.sticky").remove();
 }
